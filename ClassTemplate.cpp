ba3c694279288f057fa933b4237674a528b2f52d
Emad Rajeh <30707424+EmadRajeh@users.noreply.github.com>
2020-07-07 02:49:12 +0300

    Create ClassTemplate.cpp

---
+++ ClassTemplate.cpp
@@ -0,0 +1,69 @@
+//
+//  main.cpp
+//  Standard Template Library
+//
+//  Created by Emad Rajeh on 27/06/2020.
+//  Copyright © 2020 Emad Rajeh. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
+
+#define nline "\n"
+using namespace std;
+
+template <class T> class AddElements {
+    public:
+        T element;
+        AddElements(T i) {
+            element = i;
+        }
+        T add(T i) {
+            return element+i;
+        }
+        T concatenate(T i) {
+            return element+i;
+        }
+};
+int start_up() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
+
+int static r = start_up();
+
+#define endl '\n';
+
+int main () {
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
